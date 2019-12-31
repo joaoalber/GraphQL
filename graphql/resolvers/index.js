f0e45869228f201d888b9893df194cb036d7e18f
joaoalber <jooalberto1@gmail.com>
2019-12-31 11:05:08 -0300

    Model booking criado e relações com evento/usuario realizadas

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,7 +1,8 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
 //models
-const Event = require('../../models/event');
-const User = require('../../models/User');
+const Event = require('../../models/event')
+const User = require('../../models/user')
+const Booking = require('../../models/booking')
 
 const events = eventIds => {
     return Event.find({_id: {$in: eventIds}})
@@ -18,6 +19,21 @@ const events = eventIds => {
     .catch(err => {
        throw err
     })
+ }
+
+ const singleEvent = eventId => {
+   return Event.findById(eventId)
+   .then(event => {
+      return {
+         ...event._doc, 
+         _id: event.id, 
+         creator: user.bind(this, event.creator)
+      }
+   })
+   
+   .catch(err => {
+      throw err
+   })
  }
  
  const user = userId => {
@@ -49,7 +65,24 @@ module.exports = {
           throw err
        })
     },
-    createEvent: (args) => {
+    bookings: () => {
+      return Booking.find().then(bookings => {
+         return bookings.map(booking => {
+            return {
+               ...booking._doc, 
+               _id: booking.id, 
+               user: user.bind(this, booking._doc.user),
+               event: singleEvent.bind(this, booking._doc.event),
+               createdAt: new Date(booking._doc.createdAt).toISOString(),
+               updatedAt: new Date(booking._doc.createdAt).toISOString()
+            }
+         })
+      })
+      .catch(err => {
+         throw err
+      })
+    },
+    createEvent: args => {
       const event = new Event({
          title: args.eventInput.title,
          description: args.eventInput.description,
@@ -97,6 +130,48 @@ module.exports = {
        }).catch(err => {
           throw err
        })
-       
-    }
- }
+    },
+    bookEvent: args => {
+      return Event.findOne({_id: args.eventId})
+      .then(fetchedEvent => {
+         const booking = new Booking({
+            user: '5e077d4cf3bb7f1f7caf8010',
+            event: fetchedEvent
+         })
+         return booking.save()
+      })
+      .then(result => {
+         return { 
+            ...result._doc, 
+            _id: result.id, 
+            user: user.bind(this, result._doc.user),
+               event: singleEvent.bind(this, result._doc.event),
+            createdAt: new Date(result._doc.createdAt).toISOString(),
+            updatedAt: new Date(result._doc.createdAt).toISOString() 
+         }
+      })
+      .catch(err => {
+         throw err
+      })
+   },
+   cancelBooking: args => {
+      return Booking.findById(args.bookingId).populate('event')
+      .then(booking => {
+         return { 
+            ...booking.event._doc, 
+            _id: booking.event.id, 
+            creator: user.bind(this, booking.event._doc.creator) 
+         }
+      })
+      .then(event => {
+         return Booking.deleteOne({_id: args.bookingId}).then(() => {
+            return event
+         }).catch(err => {
+            throw err
+         })
+      })
+      .catch(err => {
+         throw err
+      })
+   }
+}
